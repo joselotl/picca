0adb79a16b043f26692c8a5851ab1161b99db546
Michael Walther <michael.walther@cea.fr>
2019-02-20 17:44:50 +0100

    back to mean

--- py/picca/Pk1D.py
+++ py/picca/Pk1D.py
@@ -197,7 +197,7 @@ def compute_cor_reso_matrix(dll, mean_reso_matrix, ll):
     k_r = sp.arange(nb_bin_FFT,dtype=float)*2*sp.pi/length_lambda_r
 
     #convert k-bins to velocity space
-    k_v=k_r/(constants.speed_light/1000.)*10**sp.median(ll)
+    k_v=k_r/(constants.speed_light/1000.)*10**sp.mean(ll)
 
     #define output k array (could be an argument instead)
     k = sp.arange(nb_bin_FFT,dtype=float)*2*sp.pi/length_lambda_v
